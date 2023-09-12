9fb47c80c6f57222541a72ab6eef649ef1a86d5d
İbrahim TULUMCU <94108626+ibrahimTlmcu@users.noreply.github.com>
2023-09-13 01:45:08 +0300

    Panel update

---
+++ WebApplication2/Controllers/KayitOlController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models.Entity;
+
+namespace WebApplication2.Controllers
+{
+    public class KayitOlController : Controller
+    {
+        // GET: KayitOl
+        DBKUTUPHANEEntitiesEnSon db = new DBKUTUPHANEEntitiesEnSon();
+        [HttpGet]
+        public ActionResult Kayit()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Kayit(TBLUYELER P) 
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Kayit");
+            }
+            
+            db.TBLUYELER.Add(P);
+            db.SaveChanges();
+            return View();
+        
+        }
+
+    }
+}
---
+++ WebApplication2/Controllers/LoginController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models.Entity;
+using System.Web.Security;
+
+namespace WebApplication2.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        DBKUTUPHANEEntitiesEnSon db = new DBKUTUPHANEEntitiesEnSon();
+        public ActionResult GirisYap()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult GirisYap(TBLUYELER p)
+        {
+            var bilgiler = db.TBLUYELER.FirstOrDefault(x => x.MAIL == p.MAIL && x.SIFRE == p.SIFRE);
+            if(bilgiler != null)
+            {
+                FormsAuthentication.SetAuthCookie(bilgiler.MAIL, false);
+                Session["Ad"] = bilgiler.AD.ToString();
+                Session["Soyad"] = bilgiler.SOYAD.ToString();
+                Session["KullaniciAdi"] = bilgiler.KULLANICIADI.ToString();
+                Session["Sifre"] = bilgiler.SIFRE.ToString();
+                Session["Ad"] = bilgiler.AD.ToString();
+                return RedirectToAction("Index","Panelim");
+                
+            }
+            else
+            {
+                return View();
+            }
+          
+        }
+    }
+}
